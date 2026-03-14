25a2c02b88b1c4381a7664ab0a00e7cdae49e6cf
Lovish Grover <groverlovish05@gmail.com>
2026-03-14 14:33:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,41 @@
 const form = document.getElementById("earlyForm");
+const loader = document.getElementById("loader");
+const btnText = document.getElementById("btnText");
+const successMessage = document.getElementById("successMessage");
 
-form.addEventListener("submit", function(e){
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
 
-e.preventDefault();
+  loader.classList.remove("hidden");
+  btnText.innerText = " ";
+  document.getElementById("submitBtn").disabled = true;
 
-const data = new FormData(form);
+  const data = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    role: document.getElementById("role").value,
+    feedback: document.getElementById("feedback").value,
+  };
 
-fetch("https://script.google.com/macros/s/AKfycbzKbrNrilKxPirdiM0krVcpSBYOTb0y42lm0q-uzhU-xSa9Rh-xlyRAAqZub8RtD7rm/exec", {
+  fetch(
+    "https://script.google.com/macros/s/AKfycbyNyQI2Eh9vaXF02bU5koCjaYSQmmaKRi-TsvUL9aelCPDPRyKqyw1AI2iBtVJkqSKM/exec",
+    {
+      method: "POST",
+      body: JSON.stringify(data),
+    },
+  )
+    .then((res) => res.text())
+    .then((response) => {
+      loader.classList.add("hidden");
 
-method: "POST",
-body: data
+      btnText.innerText = "Submitted";
 
-})
-.then(res => {
+      successMessage.classList.remove("hidden");
 
-alert("Thank you! You're on the early access list.");
-
-form.reset();
-
-})
-.catch(err => {
-
-alert("Something went wrong. Please try again.");
-
-});
-
+      form.reset();
+    })
+    .catch((error) => {
+      loader.classList.add("hidden");
+      btnText.innerText = "Try Again";
+    });
 });
